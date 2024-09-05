99ac9d18312ee13ff40a967bd6bcbbeeccab01bd
Liliya Shkabura <Liliya.Sh@fraglab.local>
2024-09-06 00:32:00 +0300

    visual refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.PNG';
 import card from './card.png';
 import guide from './guide.png';
-import map from './Map.png';
+import map from './map.png';
 import './App.css';
 import BookCard from "./components/BookCard";
 import NewBooks from './data/BooksData.json';
@@ -10,45 +10,68 @@ import PurchasedBooks from './data/ArchivedBooksData.json';
 function App() {
 
   return (
-    <div className="App">
+    <div>
       <header className="App-header" >
-        <img src={logo} alt="" style={{alignSelf: 'stretch'}}/>
+        <img src={logo} alt="" style={{alignSelf: 'stretch', maxHeight: '80vh'}}/>
       </header>
-      <p/>
 
-      <button style={{width:"85%", backgroundColor:"white", color:'black'}}>
-        <h2>Давайте знайомитись</h2>
-        <img src={card} alt="" style={{width: '70%'}}/>
-      </button>
-      <p/>
-      <button style={{width:"85%", backgroundColor:"white", color:'black'}}>
-        <h2>Про бібліотеку</h2>
-        <img src={map} height="220vh" alt=""/>
-        <p>Адреса бібліотеки: 04053, м. Київ, вул. Олександра Кониського, 83-85</p>
-        <p>е-mail: lubibl@ukr.net, тел.: (+38-044) 486-01-46</p>
-        <p>Вебресурси:</p>
-        <a style={{color:'blueviolet'}} href="https://www.facebook.com/LUkLibrary">  Facebook  </a>
-        <a style={{color:'blueviolet'}} href="https://lukl.kyiv.ua/">  Web page  </a>
-        <a style={{color:'blueviolet'}} href="https://www.tiktok.com/@lesyalibrary?_t=8oqALFN1u1W&_r=1">  Tiktok  </a>
-      </button>
-      <p/>
-      <button style={{width:"85%", backgroundColor:"white", color:'black'}}>
-        <h2>Як долучитись до проєкту</h2>
-        <img src={guide} alt="" style={{width: '70%'}}/>
-      </button>
-  
-      <div className="grid place-items-center h-screen bg-zinc-300 p-5 text-[#454545]">
+      <ul className="container">
+        <img src={card} alt="" width="30%"/>
+        <div style={{border: "solid 15px transparent"}}>
+          <h1> Давайте знайомитись</h1>
+          <h3 style={{color:"darkorange"}}>Мене звуть Ліля і це мій проєкт з лагідної українізації</h3>
+          <p>Мова йде про ініціативу з наповнення мережі бібліотек імені Лесі Українки новими книжками. </p>
+          <p>Бібліотекарки надсилають мені запити на літературу, яку просили читачі і не знайшли, а у вас є можливість придбати цю книжку для них! </p>
+          <p/>
+          <p>Може ви спитаєте для чого взагалі ще існують бібліотеки? </p>
+          <p>В бібліотеки ходить багато вимушено переселених людей, та тих хто не може тримати велику книжкову полицю вдома, часто ходять з дітками, адже дитячі книжки маленькі і швидко набридають. </p>
+          <p>Дуже хочеться щоб більше людей мало доступ до якісної літератури українською, хочеться бачити на полицях красиві сучасні книги. </p>
+          <p>Разом ми можемо це зробити.</p>
+          <p/>
+        </div>
+      </ul>
+    
+      <ul className="container"  style={{backgroundColor:'DarkSeaGreen', color:'black'}}>
+        <div style={{border: "solid 15px transparent"}}>
+          <h2>Про бібліотеку</h2>
+          <h3 style={{color:"white"}}>Публічна бібліотека імені Лесі Українки</h3>
+          <p>Адреса бібліотеки: 04053, м. Київ, вул. Олександра Кониського, 83-85</p>
+          <p>е-mail: lubibl@ukr.net, тел.: (+38-044) 486-01-46</p>
+          <h3>Вебресурси:</h3>
+          <a href="https://www.facebook.com/LUkLibrary"> Facebook </a>
+          <a href="https://lukl.kyiv.ua/"> Web page </a>
+          <a href="https://www.tiktok.com/@lesyalibrary?_t=8oqALFN1u1W&_r=1"> Tiktok </a>
+        </div>
+        <img src={map} alt="" width="30%"/>
+      </ul>
+
+      <ul className="container">
+        <img src={guide} alt="" width="50%"/>
+        <div style={{border: "solid 15px transparent"}}>
+          <h1> Як долучитись до проєкту</h1>
+          <h3 style={{color:"darkorange"}}>Мене звуть Ліля і це мій проєкт з лагідної українізації</h3>
+          <p>Мова йде про ініціативу з наповнення мережі бібліотек імені Лесі Українки новими книжками. </p>
+          <p>Бібліотекарки надсилають мені запити на літературу, яку просили читачі і не знайшли, а у вас є можливість придбати цю книжку для них! </p>
+          <p/>
+          <p>Може ви спитаєте для чого взагалі ще існують бібліотеки? </p>
+          <p>В бібліотеки ходить багато вимушено переселених людей, та тих хто не може тримати велику книжкову полицю вдома, часто ходять з дітками, адже дитячі книжки маленькі і швидко набридають. </p>
+          <p>Дуже хочеться щоб більше людей мало доступ до якісної літератури українською, хочеться бачити на полицях красиві сучасні книги. </p>
+          <p>Разом ми можемо це зробити.</p>
+          <p/>
+        </div>
+      </ul>
+      <ul className="container"  style={{backgroundColor:'white', color:'black'}}>
         <h2>Нові запити</h2>
         <ul className="w-auto">
           {NewBooks.map((data, index) => { return (<BookCard key={index} data={data} />); })}
         </ul>
-      </div>
-      <div className="grid place-items-center h-screen bg-zinc-300 p-5 text-[#454545]">
-        <h2>Передано бібліотеці</h2>
+      </ul>
+      <ul className="container"  style={{backgroundColor:'DarkSeaGreen', color:'black'}}>
         <ul className="w-auto">
           {PurchasedBooks.map((data, index) => { return (<BookCard key={index} data={data} />); })}
         </ul>
-      </div>
+        <h2>Передано бібліотеці</h2>
+      </ul>
     </div>
   );
 };
