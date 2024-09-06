336785551910e9b5d42f64c2362bb5fc131d8d72
Liliya Shkabura <Liliya.Sh@fraglab.local>
2024-09-06 11:32:39 +0300

    visual refactor3

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       </header>
 
       <ul className='container'>
-        <img src={card} alt="" width="30%"/>
-        <div style={{border: "solid 15px transparent"}}>
+        <img className="box box1" src={card} alt="" width="80wv"/>
+        <div className="box box2">
           <h1> Давайте знайомитись</h1>
           <h3 style={{color:"darkorange"}}>Мене звуть Ліля і це мій проєкт з лагідної українізації</h3>
           <p>Мова йде про ініціативу з наповнення мережі бібліотек імені Лесі Українки новими книжками. </p>
@@ -31,7 +31,7 @@ function App() {
       </ul>
     
       <ul className='container' style={{backgroundColor:'DarkSeaGreen', color:'black'}}>
-        <div style={{border: "solid 15px transparent"}}>
+        <div className="box box1" >
           <h2>Про бібліотеку</h2>
           <h3 style={{color:"white"}}>Публічна бібліотека імені Лесі Українки</h3>
           <p>Адреса бібліотеки: 04053, м. Київ, вул. Олександра Кониського, 83-85</p>
@@ -41,12 +41,12 @@ function App() {
           <a href="https://lukl.kyiv.ua/"> Web page </a>
           <a href="https://www.tiktok.com/@lesyalibrary?_t=8oqALFN1u1W&_r=1"> Tiktok </a>
         </div>
-        <img src={map} alt="" width="30%"/>
+        <img className="box box2" src={map} alt=""/>
       </ul>
 
       <ul className='container'>
-        <img src={guide} alt="" width="50%"/>
-        <div style={{border: "solid 15px transparent"}}>
+        <img className="box box1" src={guide} alt=""/>
+        <div className="box box2">
           <h1> Як долучитись до проєкту</h1>
           <h3 style={{color:"darkorange"}}>Скористатись оплатою на сайті або віднести книгу самостійно</h3>
           <p>Обирайте книгу, тисніть замовити і сплачуйте її вартість. Протягом доби кошти будуть зараховані і книга перейде в статус ЗАБРОНЬОВАНО. У випадку, якщо кілька людей встигне забронювати одну книгу, кошти буде витрачено на інші книжечки з списку. Раз на тиждень я купую книги та передаю бібліотеці. </p>
@@ -60,7 +60,7 @@ function App() {
         <h2>Нові запити</h2>
       </ul>
       
-      <ul className="w-auto">
+      <ul className="container">
         {NewBooks.map((data, index) => { return (<BookCard key={index} data={data} />); })}
       </ul>
       
@@ -68,7 +68,7 @@ function App() {
         <h2>Передано бібліотеці</h2>
       </ul>
       
-      <ul className="w-auto">
+      <ul className="container">
         {PurchasedBooks.map((data, index) => { return (<BookCard key={index} data={data} />); })}
       </ul>
     </div>
